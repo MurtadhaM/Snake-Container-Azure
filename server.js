2733458677e9c95c7120d38a6ed1a5db1655fb39
Murtadha Marzouq <mmarzou0@gmail.com>
2024-01-26 12:18:53 -0500

    main

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+
+
+app.get('/api/hello', (req, res) => {
+    let name = req.query.name || 'World';
+    res.send(`Hello ${name}!`);
+});
+
+
+app.get('/api/bye', (req, res) => {
+    let name = req.query.name || 'World';
+    res.send(`Goodbye ${name}!`);
+}
+);
+
+app.post('/api/json', (req, res) => {
+    // get the data from the request
+    let data = req.body;
+    // send the data back
+    res.json(data);
+}
+);
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 }
